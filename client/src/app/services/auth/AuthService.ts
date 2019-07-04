bb77c6e2e1883482d0a8d7187669192c204e6877
ps1 <trexbazarov@gmail.com>
2019-07-04 18:37:53 +0300

    init

---
+++ client/src/app/services/auth/AuthService.ts
@@ -0,0 +1,43 @@
+import * as moment from "moment";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {environment} from '/environments/environment'
+
+@Injectable()
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<any>(environment.apiURI + '/api/login', {email, password})
+      .do(res => this.setSession)
+      .shareReplay();
+  }
+
+  private setSession(authResult) {
+    const expiresAt = moment().add(authResult.expiresIn, 'second');
+
+    localStorage.setItem('id_token', authResult.idToken);
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+}
